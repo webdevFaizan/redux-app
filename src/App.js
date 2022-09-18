878f0f08a72dc9484e2823349ad4968dfcc16266
webdevFaizan <webdev.faizan@gmail.com>
2022-09-18 06:30:02 +0530

    Understanding redux

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Shop from './Components/Shop';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div className="container">
+        <Shop/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <>
+        <div className="container">
+            <button className="btn btn-primary mx-2">-</button>
+                Add items
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    </>
+  )
+}
